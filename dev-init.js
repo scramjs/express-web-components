38aee7a272f39c055bcbae64614eeb14a842a6bc
Jordan Last <jordan.michael.last@gmail.com>
2016-04-29 12:51:46 -0600

    dev environment working

--- dev-init.js
+++ dev-init.js
@@ -1,6 +1,6 @@
 #!/usr/bin/env node
 
-//TODO get the package name from package.json somehow
+//TODO to make this a module of its own, get the package name from package.json somehow
 
 var exec = require('child_process').exec;
 var cmd = 'mkdir -p node_modules/scram-express && ln -s ../../scram-express-app.html node_modules/scram-express/scram-express-app.html && ln -s ../../scram-express-middleware.html node_modules/scram-express/scram-express-middleware.html';
