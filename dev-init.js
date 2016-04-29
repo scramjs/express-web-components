b77b4be6ac1134399f8067afdecec3307022fde5
Jordan Last <jordan.michael.last@gmail.com>
2016-04-29 13:12:26 -0600

    changing component names

--- dev-init.js
+++ dev-init.js
@@ -3,7 +3,7 @@
 //TODO to make this a module of its own, get the package name from package.json somehow
 
 var exec = require('child_process').exec;
-var cmd = 'mkdir -p node_modules/scram-express && ln -s ../../scram-express-app.html node_modules/scram-express/scram-express-app.html && ln -s ../../scram-express-middleware.html node_modules/scram-express/scram-express-middleware.html';
+var cmd = 'mkdir -p node_modules/scram-express && ln -s ../../express-app.html node_modules/scram-express/express-app.html && ln -s ../../express-middleware.html node_modules/scram-express/express-middleware.html';
 
 var newProcess = exec(cmd);
 
