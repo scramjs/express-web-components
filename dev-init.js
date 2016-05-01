80ae8799247d383a8ad983cb8c5a732e4830527f
Jordan Last <jordan.michael.last@gmail.com>
2016-04-30 21:11:53 -0600

    renaming dev directory and handling deduping

--- dev-init.js
+++ dev-init.js
@@ -3,7 +3,12 @@
 //TODO to make this a module of its own, get the package name from package.json somehow
 
 var exec = require('child_process').exec;
-var cmd = 'mkdir -p node_modules/scram-express && ln -s ../../express-app.html node_modules/scram-express/express-app.html && ln -s ../../express-middleware.html node_modules/scram-express/express-middleware.html && ln -s ../../express-router.html node_modules/scram-express/express-router.html';
+var cmd = `
+    mkdir -p node_modules/express-web-components &&
+    ln -s ../../express-app.html node_modules/express-web-components/express-app.html &&
+    ln -s ../../express-middleware.html node_modules/express-web-components/express-middleware.html &&
+    ln -s ../../express-router.html node_modules/express-web-components/express-router.html
+`;
 
 var newProcess = exec(cmd);
 
